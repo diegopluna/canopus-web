43ba1f00ecdb55bb31bc430940fee32bfaa18010
diegoluna <dpl@cesar.school>
2023-11-11 12:45:45 -0300

    i18n in login screen

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useContext } from "react";
+import { useTranslation} from 'react-i18next';
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
@@ -15,8 +16,10 @@ interface FormValues {
     password: string;
 }
 
+//TODO: SETAR i18N
 
 export default function SignIn() {
+    const { t } = useTranslation(['signin']);
     const { toast } = useToast()
     const [formData, setFormData] = useState<FormValues>({
         email: "",
@@ -51,12 +54,12 @@ export default function SignIn() {
     return (
         <div key="1" className="min-h-screen flex flex-col justify-between">
             <img
-                alt="Background Image Top Right"
+                alt={t('bg-img-top')}
                 className="hidden lg:block absolute top-0 right-0 h-[438px] w-[457px] object-cover z-10"
                 src={starTopRight}
             />
             <img
-                alt="Background Image Bottom Left"
+                alt={t('bg-img-bottom')}
                 className="hidden lg:block absolute bottom-0 left-0 h-[580px] w-[628px] object-cover z-10"
                 src={starBottomLeft}
             />
@@ -64,7 +67,7 @@ export default function SignIn() {
                 <div className="p-4 sm:p-10  mx-auto w-full sm:w-[400px] rounded-xl">
                     <h1 className="text-center text-3xl font-bold mb-4">
                         <img
-                            alt="Logo"
+                            alt={t('logo')}
                             className="aspect-[2/1] overflow-hidden rounded-lg object-contain object-center mx-auto"
                             height="140"
                             src={canopusLogo}
@@ -73,11 +76,12 @@ export default function SignIn() {
                     </h1>
                     <form className="space-y-4" onSubmit={handleSubmit}>
                         <div className="space-y-2">
-                            <Label className="text-base" htmlFor="email">E-mail</Label>
+                            <Label className="text-base" htmlFor="email">{t("email-label")}</Label>
                             <Input 
                                 className="dark:bg-muted/30 dark:border-input/30 dark:text-primary-foreground" 
                                 id="email"
-                                name="email" 
+                                name="email"
+                                placeholder={t("email-placeholder")} 
                                 required 
                                 type="email"
                                 value={formData.email}
@@ -85,28 +89,29 @@ export default function SignIn() {
                             />
                         </div>
                         <div className="space-y-2">
-                            <Label className="text-base" htmlFor="password">Senha</Label>
+                            <Label className="text-base" htmlFor="password">{t("password-label")}</Label>
                             <Input 
                                 className="dark:bg-muted/30 dark:border-input/30 dark:text-primary-foreground" 
                                 id="password" 
                                 name="password"
                                 required 
+                                placeholder={t("password-placeholder")}
                                 type="password" 
                                 value={formData.password}
                                 onChange={handleInputChange}
                             />
                         </div>
                         <Button className="w-full text-lg" type="submit">
-                            Entrar
+                            {t("signin-button")}
                         </Button>
                     </form>    
                 </div>  
                 <Separator className="my-4 w-full sm:w-[400px] mx-auto dark:bg-[#FFFFFF]/50" /> 
                 <div className="text-center">
-                    <span className="dark:text-white text-lg">Não possui conta? <a className="dark:text-foreground" href="#">Cadastre-se</a></span>
+                    <span className="dark:text-white text-lg">{t("signup-message")} <a className="dark:text-foreground" href="#">{t("signup-button")}</a></span>
                 </div>
                 <div className="my-8 text-center ">
-                    <span className="dark:text-[#64748B] text-base">Esqueceu sua senha? Contate nossos moderadores pelo e-mail<br />moderador@gmail.com</span>
+                    <span className="dark:text-[#64748B] text-base">{t("forgot-password")}<br />moderador@gmail.com</span>
                 </div>
             </main>
             <Footer />
